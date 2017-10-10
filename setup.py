30aee97b83e66f34b48b099cf6d466bef2afb2ac
Jan Willhaus <mail@janwillhaus.de>
2017-10-10 09:50:41 +0200

    Updated distribution metadata

--- setup.py
+++ setup.py
@@ -1,17 +1,38 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# Get the long description from the README file
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
-from setuptools import setup
 setup(
     name='markdown-figure',
     version='0.0.1',
+
     author='Jan Willhaus',
     author_email='mail@janwillhaus.de',
+
     description='Markdown extension which turns images into figures',
+    long_description=long_description,
+
     url='https://github.com/janwh/markdown-figures',
+    license='MIT',
+    keywords='markdown extension html figure',
+
     py_modules=['mdfigure'],
+    python_requires='>=3',
     install_requires=['Markdown>=2.0'],
     classifiers=[
-        'Topic :: Text Processing :: Markup :: HTML'
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
     ]
 )
