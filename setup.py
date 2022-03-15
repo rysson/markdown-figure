5d46bb57552c2f2c4f38431a4f6d30143fb3cbca
rysson <robert.kalinowski@sharkbits.com>
2022-03-15 11:41:24 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='markdown-figure',
-    version='0.0.1',
+    version='0.0.2',
 
     author='Jan Willhaus',
     author_email='mail@janwillhaus.de',
